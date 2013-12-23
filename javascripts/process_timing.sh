4b16f1d26ebcf6a4ffc6338c8aad5b082b6a08d7
Kevin Manalo <kmanalo@gmail.com>
2013-12-23 02:03:39 -0600

    adding process_timing.sh

---
+++ javascripts/process_timing.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+echo "./timestamp/timestamp_render.py > timestamp/timestamp.json"
+echo "./xchart/xchart_render.py > xchart/xchart.json"
+./timestamp/timestamp_render.py > timestamp/timestamp.json
+./xchart/xchart_render.py > xchart/xchart.js
