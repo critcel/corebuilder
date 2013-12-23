e44d845c81081bb0f14eaa8492846022997da205
Kevin Manalo <kmanalo@gmail.com>
2013-12-23 01:41:00 -0600

    typo on timestamp

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () { // load json file using jquery ajax
     $.getJSON('javascripts/timestamp.json', function (data) {
         var output = '<ul>';
         $.each(data, function (key, val) {
-            output += '<li>' + val.name + ':' + '</li>';
+            output += '<li>' + val.file + ':' + val.time + '</li>';
         });
         output += '</ul>';
         $('#update').html(output);  // replace all existing content
