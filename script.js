b45ab2c910826df397abf9212e2b4b663950956f
Kevin Manalo <kmanalo@gmail.com>
2013-12-22 23:49:28 -0600

    Another go at inserting json data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () { // load json file using jquery ajax
     $.getJSON('data.json', function (data) {
         var output = '<ul>';
         $.each(data, function (key, val) {
-            output += '<li>' + val.name + ': ' + val.age '</li>';
+            output += '<li>' + val.name '</li>';
         });
         output += '</ul>';
         $('#update').html(output);  // replace all existing content
