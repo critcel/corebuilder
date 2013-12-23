cb7a99555480f37373e3da16a2091365d90e90c3
Kevin Manalo <kmanalo@gmail.com>
2013-12-23 02:14:11 -0600

    xcharts.js had to move up a level...

--- javascripts/process_timing.sh
+++ javascripts/process_timing.sh
@@ -3,4 +3,4 @@
 echo "./timestamp/timestamp_render.py > timestamp/timestamp.json"
 echo "./xchart/xchart_render.py > xchart/xchart.json"
 ./timestamp/timestamp_render.py > timestamp/timestamp.json
-./xchart/xchart_render.py > xchart/xchart.js
+./xchart/xchart_render.py > xchart.js
