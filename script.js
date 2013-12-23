0e6a273f228e41ab3352436fb6b996a7dc494bdf
Kevin Manalo <kmanalo@gmail.com>
2013-12-22 22:33:15 -0600

    testing json file

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () { // load json file using jquery ajax
+    $.getJSON('data.json', function (data) {
+        var output = '<ul>';
+        $.each(data, function (key, val) {
+            output += '<li>' + val.name + '</li>';
+        });
+        output += '</ul>';
+        $('#update').html(output);  // replace all existing content
+    });
+});
